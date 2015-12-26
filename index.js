2e6cb62727302e93f53a63b7fb2a116b14493652
Marko <markoprelevik@gmail.com>
2015-12-26 20:03:11 +0100

    voxel demo.

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
 var createGame = require('voxel-engine');
 var game = createGame();
 
+var container = document.body;
+game.appendTo(container);
+
+var createPlayer = require('voxel-player')(game);
+
+var dude = createPlayer();
+dude.possess();
+dude.yaw.position.set(0, 100, 0);
